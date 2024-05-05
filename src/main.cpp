a92a759a33ceb8ad693167a7975fef8819152ee6
sebnow91 <sebnow91@gmail.com>
2024-05-05 21:10:09 +0200

    Add working filter

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,54 @@
 #include <opencv2/core/types.hpp>
 #include <opencv4/opencv2/opencv.hpp>
 #include "image.hpp"
+#include "pixel.hpp"
+
+#define KERNEL_WIDTH 5
+#define KERNEL_HEIGHT 5
+
+constexpr double KERNEL[KERNEL_HEIGHT][KERNEL_WIDTH] =
+{
+  -1, -1, -1, -1, -1,
+  -1,  2,  2,  2, -1,
+  -1,  2,  8,  2, -1,
+  -1,  2,  2,  2, -1,
+  -1, -1, -1, -1, -1,
+};
+
+constexpr double FACTOR = 1. / 8.;
+
+void sharpeningFilter(BGRPixel *pixels, BGRPixel *outputImage, int width, int height) {
+    for (int y = 0; y < height; y++) {
+	for (int x = 0; x < width; x++) {
+	    
+	    double sumR = 0;
+	    double sumG = 0;
+	    double sumB = 0;
+	    
+	    for (int i = 0; i < KERNEL_HEIGHT; i++) {
+		for (int j = 0; j < KERNEL_WIDTH; j++) {
+		    int pixelX = (x + j - KERNEL_WIDTH / 2) % width;
+		    int pixelY = y + i - KERNEL_HEIGHT / 2 % height;
+		    
+		    if (pixelX >= 0 && pixelX < width && pixelY >= 0 && pixelY < height) {
+			auto pixel = pixels[pixelY * width + pixelX].pixel;
+			sumR += pixel.r * KERNEL[i][j];
+			sumG += pixel.g * KERNEL[i][j];
+			sumB += pixel.b * KERNEL[i][j];
+		    }
+		}
+	    }
+	    
+	    BGRPixel pixel;
+	    pixel.pixel.r = std::min(std::max((int) (sumR * FACTOR), 0), 255);
+	    pixel.pixel.g = std::min(std::max((int) (sumG * FACTOR), 0), 255);
+	    pixel.pixel.b = std::min(std::max((int) (sumB * FACTOR), 0), 255);
+	    
+	    outputImage[y * width + x] = pixel;
+	}
+    }
+}
+
 
 int main(int argc, char *argv[]) {
     auto inputPath = argv[1];
@@ -21,11 +69,13 @@ int main(int argc, char *argv[]) {
         return -1;
     }
 
-    //BGRPixel *pixels = intoBGRPixelArray1D(bgrImage);
+    
+    BGRPixel *pixels = intoBGRPixelArray1D(bgrImage);
     BGRPixel *outputPixels = allocateBGRPixelArray1D(size);
+    sharpeningFilter(pixels, outputPixels, size.width, size.height);
 
     auto outputMat = fromBGRPixelArray1D(outputPixels, size);
-    cv::imwrite(outputPath, bgrImage);
+    cv::imwrite(outputPath, outputMat);
     
     return 0;
 }

4875b6c84984b658e7a68499b8d0f001c609f67d
sebnow91 <sebnow91@gmail.com>
2024-05-05 22:00:57 +0200

    fix line, add another image

--- src/main.cpp
+++ src/main.cpp
@@ -30,7 +30,7 @@ void sharpeningFilter(BGRPixel *pixels, BGRPixel *outputImage, int width, int he
 	    for (int i = 0; i < KERNEL_HEIGHT; i++) {
 		for (int j = 0; j < KERNEL_WIDTH; j++) {
 		    int pixelX = (x + j - KERNEL_WIDTH / 2) % width;
-		    int pixelY = y + i - KERNEL_HEIGHT / 2 % height;
+		    int pixelY = (y + i - KERNEL_HEIGHT / 2) % height;
 		    
 		    if (pixelX >= 0 && pixelX < width && pixelY >= 0 && pixelY < height) {
 			auto pixel = pixels[pixelY * width + pixelX].pixel;
